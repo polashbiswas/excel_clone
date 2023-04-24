47673201934f0c19642d6860af08585505629962
Polash Biswas <polashbiswasit19@gmail.com>
2023-04-25 02:11:49 +0530

    add functionallity like copy paste left right color font

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const tbody = document.getElementById("table-body");
 const columns = 26;
 const rows = 100;
 let currCel;
+let cutValue = {};
+
+const boldBtn = document.getElementById("bold-btn");
+const italicsBtn = document.getElementById("italics-btn");
+const underlineBtn = document.getElementById("underline-btn");
+
+const textColor = document.getElementById("text-color");
+const bgColor = document.getElementById("bg-color");
+
+const leftAlign = document.getElementById("left-align");
+const rightAlign = document.getElementById("right-align");
+const centerAlign = document.getElementById("center-align");
+
+const fontSize = document.getElementById("font-size");
+const fontFamily = document.getElementById("font-family");
+
+const cutBtn = document.getElementById("cut-btn");
+const copyBtn = document.getElementById("copy-btn");
+const pasteBtn = document.getElementById("paste-btn");
 
 for(let column = 0; column < columns; column++){
     var th = document.createElement("th");
@@ -20,6 +39,7 @@ for(let row = 0 ; row < rows; row++){
     for(let col = 0; col < columns; col++){
         let td = document.createElement("td");
         td.setAttribute("contenteditable", "true");
+        td.setAttribute("spellcheck", "false");
         td.setAttribute("id", `${String.fromCharCode(65 + col)}${row + 1}`);
         td.addEventListener("focus", (event) => onFocusFnc(event));
         tr.appendChild(td);
@@ -33,3 +53,78 @@ function onFocusFnc(event){
     currCel = event.target;
     document.getElementById("current-cell").innerText = event.target.id;
 }
+
+
+//event listeners for button
+boldBtn.addEventListener("click", () => {
+    if(currCel.style.fontWeight == "bold"){
+        currCel.style.fontWeight = "normal";
+    }else{
+        currCel.style.fontWeight = "bold";
+    }
+});
+
+italicsBtn.addEventListener("click", () => {
+    if(currCel.style.fontStyle == "italic"){
+        currCel.style.fontStyle = "normal";
+    }else{
+        currCel.style.fontStyle = "italic";
+    }
+});
+
+underlineBtn.addEventListener("click", () => {
+    if(currCel.style.textDecoration == "underline"){
+        currCel.style.textDecoration = null;
+    }else{
+        currCel.style.textDecoration = "underline";
+    }
+});
+
+
+bgColor.addEventListener("change", () => {
+    currCel.style.backgroundColor = bgColor.value;
+})
+
+textColor.addEventListener("change", () => {
+    currCel.style.color = textColor.value;
+})
+
+
+leftAlign.addEventListener("click", () => {
+    currCel.style.textAlign = "left";
+});
+rightAlign.addEventListener("click", () => {
+    currCel.style.textAlign = "right";
+});
+centerAlign.addEventListener("click", () => {
+    currCel.style.textAlign = "center";
+});
+
+fontSize.addEventListener("change", () => {
+    currCel.style.fontSize = fontSize.value;
+});
+
+fontFamily.addEventListener("change", () => {
+    currCel.style.fontFamily = fontFamily.value;
+});
+
+cutBtn.addEventListener("click", () =>{
+    cutValue = {
+        style: currCel.style.cssText,
+        text: currCel.innerText
+    };
+    currCel.style = null;
+    currCel.innerText = null;
+});
+
+copyBtn.addEventListener("click", () =>{
+    cutValue = {
+        style: currCel.style.cssText,
+        text: currCel.innerText
+    };
+});
+
+pasteBtn.addEventListener("click", () =>{
+    currCel.style.cssText = cutValue.style;
+    currCel.innerText = cutValue.text;
+});
